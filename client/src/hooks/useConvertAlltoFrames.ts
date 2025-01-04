6d37d4fdf222ed3cef2ab305ef377b70674cf65b
iamDuckduck <simonchungCCK42@gmail.com>
2025-01-04 12:08:36 +0800

    awaitMutation, send convert request 1 by 1

--- client/src/hooks/useConvertAlltoFrames.ts
+++ client/src/hooks/useConvertAlltoFrames.ts
@@ -1,15 +1,30 @@
 import { useAcceptedFileStore } from "../store";
 import useGiftoFrames from "../hooks/useGiftoFrames";
+import { FileStatus } from "../entities/fileStatus";
 
 const useConvertAlltoFrames = () => {
   const files = useAcceptedFileStore((s) => s.files); //stores accpetedFiles
-  const { mutate } = useGiftoFrames();
+  const setUpdateFiles = useAcceptedFileStore((s) => s.setUpdateFiles);
+  const { mutateAsync } = useGiftoFrames();
 
-  return () => {
-    files.forEach((file) => {
-      mutate(file);
+  return async () => {
+    const updatedFiles = files.map((file) => {
+      return Object.assign(file, { status: FileStatus.CONVERTING });
     });
+    setUpdateFiles(updatedFiles);
+
+    for (const file of files) {
+      await mutateAsync(file);
+    }
   };
+
+  // return () =>
+  //   useEffect(() => {
+  //     const matchedFile = files.find(
+  //       (file) => file.status === FileStatus.UPLOADED
+  //     );
+  //     if (matchedFile) mutate(matchedFile);
+  //   }, [files]);
 };
 
 export default useConvertAlltoFrames;
