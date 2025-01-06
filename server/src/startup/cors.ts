62893fa54a13b9bfea5e71c5c75986be8a31de6d
iamDuckduck <simonchungCCK42@gmail.com>
2025-01-06 23:50:20 +0800

    test

--- server/src/startup/cors.ts
+++ server/src/startup/cors.ts
@@ -2,14 +2,13 @@ import { Express } from "express";
 import cors from "cors";
 
 export const enableCors = function (app: Express) {
-  console.log(process.env.NODE_ENV);
   if (process.env.NODE_ENV == "development") {
     // Set up CORS with custom options
     const corsOptions = {
       origin:
         process.env.NODE_ENV == "development"
           ? "http://localhost:5173"
-          : "https://anime-hub-one-chi.vercel.app",
+          : "https://cursor-frames-client.vercel.app",
       // Allow requests from this origin
       methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
       //   credentials: true,
