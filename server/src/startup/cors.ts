e8d31ffb0a95b07ce31354737b69d1b462d70b19
iamDuckduck <simonchungCCK42@gmail.com>
2025-01-07 02:04:42 +0800

    cors minor adjust

--- server/src/startup/cors.ts
+++ server/src/startup/cors.ts
@@ -2,17 +2,11 @@ import { Express } from "express";
 import cors from "cors";
 
 export const enableCors = function (app: Express) {
-  // if (process.env.NODE_ENV == "development") {
-  //   console.log(process.env.NODE_ENV);
-  // Set up CORS with custom options
-  const corsOptions = {
-    origin:
-      process.env.NODE_ENV == "development"
-        ? "http://localhost:5173"
-        : "https://cursor-frames-client.vercel.app",
-    // Allow requests from this origin
-    methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
-  };
-  app.use(cors(corsOptions));
-  // }
+  if (process.env.NODE_ENV == "development") {
+    const corsOptions = {
+      origin: "http://localhost:5173",
+      methods: "GET,PUT,POST,DELETE",
+    };
+    app.use(cors(corsOptions));
+  }
 };
