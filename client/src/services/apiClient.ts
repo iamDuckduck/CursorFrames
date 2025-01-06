e845702fa61a38d93e5224c6568991b4e5b4fd32
iamDuckduck <simonchungCCK42@gmail.com>
2025-01-06 23:18:19 +0800

    minor adjust on axiosInstance and add specific headers to both post request

--- client/src/services/apiClient.ts
+++ client/src/services/apiClient.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { acceptedFile } from "../entities/acceptedFile";
+import { aniPostObject } from "../entities/aniPostObject";
+
+const baseURL =
+  import.meta.env.VITE_ENV !== "production"
+    ? "http://localhost:3000/api/"
+    : import.meta.env.VITE_BACK_END;
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.VITE_ENV !== "production"
-      ? "http://localhost:3000/api/"
-      : import.meta.env.VITE_BACK_END,
-  headers: { "Content-Type": "multipart/form-data" }, //send
+  baseURL: baseURL,
   responseType: "blob",
 });
 
@@ -17,10 +19,22 @@ class APIClient {
     this.endpoint = endpoint;
   }
 
-  framesPost = (data: acceptedFile) => {
+  gifToFramesPost = (data: acceptedFile) => {
     const formData = new FormData(); //wrap the file with FormData
     formData.append("file", data);
-    return axiosInstance.post(this.endpoint, formData).then((res) => res.data); // return the blob data here
+    return axiosInstance
+      .post(this.endpoint, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((res) => res.data); // return the blob data here
+  };
+
+  aniToFramesPost = (aniObject: aniPostObject) => {
+    return axiosInstance
+      .post(this.endpoint, aniObject, {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((res) => res.data);
   };
 }
 
