f1392bbf8519bcbe6f3fe648a16e6a627fa59038
iamDuckduck <simonchungCCK42@gmail.com>
2025-01-07 01:44:55 +0800

    test

--- server/src/startup/cors.ts
+++ server/src/startup/cors.ts
@@ -2,17 +2,18 @@ import { Express } from "express";
 import cors from "cors";
 
 export const enableCors = function (app: Express) {
-  if (process.env.NODE_ENV == "development") {
-    // Set up CORS with custom options
-    const corsOptions = {
-      origin:
-        process.env.NODE_ENV == "development"
-          ? "http://localhost:5173"
-          : "https://cursor-frames-client.vercel.app",
-      // Allow requests from this origin
-      methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
-      //   credentials: true,
-    };
-    app.use(cors(corsOptions));
-  }
+  // if (process.env.NODE_ENV == "development") {
+  //   console.log(process.env.NODE_ENV);
+  // Set up CORS with custom options
+  const corsOptions = {
+    origin:
+      process.env.NODE_ENV == "development"
+        ? "http://localhost:5173"
+        : "https://cursor-frames-client.vercel.app",
+    // Allow requests from this origin
+    methods: "GET,PUT,POST,DELETE", // Allow these HTTP methods
+    //   credentials: true,
+  };
+  app.use(cors(corsOptions));
+  // }
 };
