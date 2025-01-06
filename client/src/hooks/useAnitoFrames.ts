09dda42d382848c53d9479dc48d8948918c2fad2
iamDuckduck <simonchungCCK42@gmail.com>
2025-01-06 14:59:31 +0800

    create ani to frames mutation

---
+++ client/src/hooks/useAnitoFrames.ts
@@ -0,0 +1,48 @@
+import { useMutation } from "@tanstack/react-query";
+import APIClient from "../services/apiClient";
+import { AxiosError } from "axios";
+import { useAcceptedFileStore } from "../store";
+import { FileStatus } from "../entities/fileStatus";
+import { updateFileStatus } from "./useConvertAlltoFrames";
+import { acceptedFile } from "../entities/acceptedFile";
+
+const apiClent = new APIClient("/toFrames/aniToFrames");
+
+const useAnitoFrames = () => {
+  const files = useAcceptedFileStore((s) => s.files); //stores accpetedFiles
+  const setUpdateFiles = useAcceptedFileStore((s) => s.setUpdateFiles);
+
+  return useMutation<any, AxiosError, acceptedFile>({
+    mutationFn: (fileToConvert: acceptedFile) => {
+      const updatedFiles = updateFileStatus(
+        files,
+        fileToConvert,
+        FileStatus.CONVERTING
+      );
+      setUpdateFiles(updatedFiles);
+      return apiClent.framesPost(fileToConvert);
+    },
+    onSuccess(convertedFile: Blob, fileToConvert: acceptedFile) {
+      const updatedFiles = updateFileStatus(
+        files,
+        fileToConvert,
+        FileStatus.SUCCESS,
+        convertedFile
+      );
+      setUpdateFiles(updatedFiles);
+    },
+    onError(errorMsg: AxiosError<unknown, any>, fileToConvert: acceptedFile) {
+      //set file property status as error..?
+      const updatedFiles = updateFileStatus(
+        files,
+        fileToConvert,
+        FileStatus.ERROR,
+        undefined,
+        errorMsg.message
+      );
+      setUpdateFiles(updatedFiles);
+    },
+  });
+};
+
+export default useAnitoFrames;
